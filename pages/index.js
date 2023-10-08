013d77904aace6aefa8b9e66bc25e51d51bb4cc4
Makotox0207 <xiaoxishicheng@gmail.com>
2023-10-08 22:19:13 +0900

    トップページのスタイリング

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,65 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Layout>テスト</Layout>
+      <Layout>
+        <section className={utilStyles.headingMd}>
+          <p>
+            テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テテスト。テスト。テスト。テスト。
+            テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テテスト。テスト。テスト。テスト。
+          </p>
+        </section>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2>ブログ</h2>
+          <div className={styles.grid}>
+            <article>
+              <Link href="/">
+                <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+              </Link>
+              <Link href="/" className={utilStyles.boldText}>
+                hogehgoefugafaugafuaga
+              </Link>
+              <br />
+              <small>February 23, 2020</small>
+            </article>
+            <article>
+              <Link href="/">
+                <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+              </Link>
+              <Link href="/" className={utilStyles.boldText}>
+                hogehgoefugafaugafuaga
+              </Link>
+              <br />
+              <small>February 23, 2020</small>
+            </article>
+            <article>
+              <Link href="/">
+                <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+              </Link>
+              <Link href="/" className={utilStyles.boldText}>
+                hogehgoefugafaugafuaga
+              </Link>
+              <br />
+              <small>February 23, 2020</small>
+            </article>
+            <article>
+              <Link href="/">
+                <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+              </Link>
+              <Link href="/" className={utilStyles.boldText}>
+                hogehgoefugafaugafuaga
+              </Link>
+              <br />
+              <small>February 23, 2020</small>
+            </article>
+          </div>
+        </section>
+      </Layout>
     </div>
   )
 }
