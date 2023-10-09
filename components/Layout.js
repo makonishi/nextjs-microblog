d17b9a52f8d73dd6a844fc9e6b61e476aadf3b31
Makotox0207 <xiaoxishicheng@gmail.com>
2023-10-09 17:04:51 +0900

    ホームボタン追加

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 const name = "makonishi";
 export const siteTitle = "Next.js blog";
@@ -24,9 +25,12 @@ function Layout({ children, home }) {
           </>
         )}
       </header>
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">← ホームへ戻る</Link>
+        </div>
+      )}
     </div>
   );
 }
